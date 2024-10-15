c31e8f35f71bcfa264c51ae18219c05fd20c3d59
Andrew Mcgallian <andymcgallian@gmail.com>
2024-10-15 14:08:39 -0500

    I lied, this should now be the last README update for awhile

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 '''
-M-TEMP DA
+M-TEMP DATA PROCESSING PROGRAM
+
+Last Updated: 10/15/2024
+Author: Andrew McGallian
+
+Here is the main file for processing M-TEMP data. The main loop for the program
+will run here and use the mtemp module's functions while running. Refer to the
+README for further guidance on operating the program.
+'''
+
+# IMPORTS
+
+import mtemp as mt
+
+# MAIN CODE
+
+def main():
+    return None
+
+# RUNNING THE PROGRAM
+
+if __name__ == "__main__":
+    main()
