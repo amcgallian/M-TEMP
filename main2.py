a8663ded8d8bf9f03388b89f69424685c7f7abb8
Andrew Mcgallian <andymcgallian@gmail.com>
2024-10-17 14:49:22 -0500

    Made it so it can be invoked from command line

--- main2.py
+++ main2.py
@@ -3,11 +3,11 @@ import mtemp2
 
 ### Uncomment out which format to use ###
 # Excel Sheet
-'''
+
 test_num = 1 # THIS WOULD BE THE ONLY INPUT WITH AN EXCEL SHEET
 tests = mtemp2.load_excelsheet()
 folder, file = mtemp2.load_test(test_num, tests)
-'''
+
 
 # Explicit Path
 '''
@@ -15,6 +15,10 @@ folder = 'path/to/folder'
 file = 'path/to/file'
 '''
 
-headers = mtemp2.get_headers(folder, file)
-daqdf = mtemp2.load_temp_daq(folder, file, headers)
-mtemp2.timeseries(daqdf) #THIS HAS A BUNCH MORE PARAMETERS, REFER TO MODULE
+def main():
+    headers = mtemp2.get_headers(folder, file)
+    daqdf = mtemp2.load_temp_daq(folder, file, headers)
+    mtemp2.timeseries(daqdf) #THIS HAS A BUNCH MORE PARAMETERS, REFER TO MODULE
+
+if __name__ == "__main__":
+    main()
